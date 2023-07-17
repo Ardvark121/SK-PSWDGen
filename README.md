b68e71f04c55e9a92907a09cbaa7e1eed865c0b5
Shawn Kim <kimshawnj@gmail.com>
2023-07-16 21:16:54 -0500

    Basic Password generator made

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # SK-PSWDGen
+
+# This is a passowrd generator that prompts what the passwords requirments are and outputs a random password meeting those requirments
+
+# Contact Me
+
+# Email:kimshawnj@gmail.com Github: Ardvark121
--- script.js
+++ script.js
@@ -10,5 +10,17 @@ function writePassword() {
   passwordText.value = password;
 }
 
+function generatePassword() {
+  var Characters =
+    "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var passwordgen = "";
+  var passwordLength = prompt("choose a number 8-128", "#");
+  for (var i = 0; i <= passwordLength; i++) {
+    var randomNumber = Math.floor(Math.random() * Characters.length);
+    passwordgen += Characters.substring(randomNumber, randomNumber + 1);
+  }
+  return passwordgen;
+}
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
